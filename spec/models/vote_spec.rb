12e9f68d90dbbf9eca5eec416931bdffee70351c
zapad7715 <zapad7715@ya.ru>
2015-09-10 15:23:46 +0300

    Remove redundant code

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,20 +3,10 @@ require 'rails_helper'
 RSpec.describe Vote, type: :model do
   describe 'validations' do
     it { should validate_inclusion_of(:value).in_array([-1, 1]) }
-    context 'for Answer' do
+    context 'for answer' do
       let(:answer){ create(:answer) }
       before { create(:vote, votable: answer) }
       it do 
-        should validate_uniqueness_of(:votable_type).scoped_to(:votable_id)
-      end 
-      it do
-        should validate_uniqueness_of(:votable_id).scoped_to(:votable_type)
-      end 
-    end
-    context 'for Question' do
-      let(:question){ create(:question) }
-      before { create(:vote, votable: question) }
-      it do
         should validate_uniqueness_of(:votable_type).scoped_to(:votable_id)
       end 
       it do
